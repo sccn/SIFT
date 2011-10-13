6e4a4f2a86bcd09e1b5ee1e5b7cf98aece0f0e37
Tim Mullen <tmullen@ucsd.edu>
2011-10-13 02:15:43 -0700

    major revision 0.8a

---
+++ external/bcilab/keywords/mandatory.m
@@ -0,0 +1,3 @@
+function res = mandatory
+% special keyword (constant function) that can be used in arg*() specifications, in particular for the default values of arguments that must be specified by the caller.
+res = '__arg_mandatory__';
