2823b0ff8a4093d0f6752df4b3a1753315b73023
Tim Mullen <mullen.tim@gmail.com>
2013-02-18 12:55:20 -0800

    minor tweaks

--- hlp/hlp_compressTimeSeries.m
+++ hlp/hlp_compressTimeSeries.m
@@ -1,11 +1,15 @@
-function signal = hlp_compressTimeSeries(signal,tsfields,newtype)
+function signal = hlp_compressTimeSeries(signal,tsfields,newtype,remfields)
 % tsfields: a cell array of fields to 'compress' by averaging across cols
 %           (after taking the absolute value)
 % signal: an eeg data structure
 % newtype: new datatype to cast to
+% remfields: fields to remove
 
 if nargin<3
     newtype = '';
+end
+if nargin<4
+    rmfields = {};
 end
 
 for fn=tsfields
@@ -16,6 +20,8 @@ for fn=tsfields
         signal.(f) = cast(signal.(f),newtype);
     end
 end
+
+signal = rmfield(signal,remfields);
 
 signal.xmax          = signal.xmin;
 signal.pnts          = 1;
