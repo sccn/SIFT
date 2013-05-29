5f574ed778ac22074e59d50a6344158f045eab64
Tim Mullen <mullen.tim@gmail.com>
2013-05-29 15:33:02 -0700

    added moving covariance option

---
+++ experimental/Online/onl_init_SIFT.m
@@ -0,0 +1,51 @@
+function EEG = onl_init_SIFT(EEG,curComps,curComponentNames,usechannels,goodchannels)
+
+    if nargin<4
+        usechannels = false;
+    end
+    
+    EEG.CAT.MODEL               = [];
+    EEG.CAT.curComps            = curComps;
+    EEG.CAT.curComponentNames   = curComponentNames;
+    EEG.CAT.nbchan              = length(curComps);
+    EEG.times                   = linspace(EEG.xmin*1000,EEG.xmax*1000,EEG.pnts);
+    
+    if usechannels
+        
+        % use identity matrices for ICA solution
+        [EEG.icaweights EEG.icasphere EEG.icawinv] = deal(eye(size(EEG.data,1)));
+        
+        % copy data into EEG.icaact and EEG.CAT.srcdata
+        [EEG.CAT.srcdata EEG.icaact] = deal(EEG.data(curComps,:));
+
+        % create dipfit structure for plotting 3D channel locations
+        % (NOTE: this should be done in run_readmindo)
+        EEG.dipfit.hdmfile  = '';
+        EEG.dipfit.mrifile  = '';
+        EEG.dipfit.chanfile = '';
+        EEG.dipfit.chansel  = 1:EEG.nbchan;
+        EEG.dipfit.coordformat = 'Spherical';
+        EEG.dipfit.coord_transform = [0 0 0 0 0 0 804.9000 1025 784.9000];
+                
+        for i=1:EEG.nbchan
+            EEG.dipfit.model(i).posxyz   = [EEG.chanlocs(i).X EEG.chanlocs(i).Y EEG.chanlocs(i).Z]*900;
+            EEG.dipfit.model(i).momxyz  = [0.1 0.1 0.1];
+            EEG.dipfit.model(i).rv      = 0.1;
+            EEG.dipfit.model(i).select  = 1;
+            EEG.dipfit.model(i).diffmap = [];
+            EEG.dipfit.model(i).sourcepot = [];
+            EEG.dipfit.model(i).datapot  = [];
+        end
+    else
+        
+         if ~all(goodchannels)
+             % recompute ica activations from only the good channels
+             icawinv = EEG.icawinv(goodchannels,:);
+             icaweights = pinv(icawinv);
+             [EEG.CAT.srcdata EEG.icaact] = deal(icaweights(curComps,:)*EEG.data(goodchannels,:));
+         end
+         
+        [EEG.CAT.srcdata EEG.icaact] = deal(EEG.icaweights(curComps,goodchannels)*EEG.data(goodchannels,:));
+        
+    end
+   
---
+++ hlp/hlp_expWinMovVar.m
@@ -0,0 +1,53 @@
+function [newVar newMean] = hlp_expWinMovVar(varargin)
+% this function implements an exponential window moving variance to
+% adapt a value based on incoming data
+%
+% algorithm:
+% diff := x - mean 
+% incr := alpha * diff 
+% mean := mean + incr 
+% variance := (1 - alpha) * (variance + diff * incr)
+
+g=arg_define([0 4],varargin, ...
+    arg_norep({'values'},mandatory,[],'data values'), ...
+    arg_norep({'lastVar','LastVar'},mandatory,[],'previous estimate of the variance'), ...
+    arg_norep({'lastMean','LastMean'},mandatory,[],'previous estimate of the mean'), ...
+    arg_norep({'numberOfRunsSoFar'},mandatory,[],'number of previous calls to this function'), ...
+    arg_norep({'threshold','AbsThresh'},[],[],'Absolute Threshold. Ignore update if absval of the new value exceeds the previous by this factor'), ...
+    arg({'mode','Mode'},'cov',{'cov','var'},'Estimate variance or covariance matrix'), ...
+    arg({'adaptationHL'},10,[0 Inf],'half-life of exponential win moving average. In frames'), ...
+    arg({'updateInterval'},1,[],'num frames between updates'), ...
+    arg({'bufferTime'},10,[],'num frames to wait before adapting') ...
+    );
+
+
+% memory factor for exponential window
+MEMFACTOR = 2/((g.adaptationHL * 2.8854)+1);
+newVar  = g.lastVar;
+newMean = g.lastMean;
+
+if (g.numberOfRunsSoFar < g.bufferTime || mod(g.numberOfRunsSoFar,g.updateInterval) == 0) && g.numberOfRunsSoFar < Inf
+    
+    if g.numberOfRunsSoFar < g.bufferTime
+        newVar  = g.lastVar;
+        newMean = g.values;
+    else
+        % apply exponential-window moving variance
+        
+%         % get the running mean
+%         mu   = hlp_expWinMovAvg(g);
+        % get the deviation
+        diff = g.values-g.lastMean;
+        % get the increment
+        incr = MEMFACTOR * diff;
+        % adjust the mean
+        newMean = g.lastMean + incr;
+        % compute variance
+        if strcmpi(g.mode,'var')
+            newVar  = (1-MEMFACTOR) * (g.lastVar + diff.*incr);
+        elseif strcmpi(g.mode,'cov')
+            newVar  = (1-MEMFACTOR) * (g.lastVar + diff*incr');
+        end
+    end
+    
+end
