0bdc598d0525aa87e5275454df9e6ace5c7dfe3c
Tim Mullen <mullen.tim@gmail.com>
2013-08-15 15:50:17 -0700

    first commit

--- external/ssm-1.0.1/ssm-release/@ssfunc/getvertmask.m
+++
@@ -1,9 +0,0 @@
-function vertmask = getvertmask(f)
-
-%@SSFUNC/GETVERTMASK Get vertical mask.
-
-% (c) 2006-2007 Jyh-Ying Peng ´^´¼·­
-% $Revision 1.0.0 $ $Generated: 2007/09/04 $
-
-vertmask    = f.vertmask;
-
