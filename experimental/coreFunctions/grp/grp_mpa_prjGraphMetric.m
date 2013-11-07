24108b2144a8a1a79746601c818b54e41a60f85d
Tim Mullen <tim@sccn.ucsd.edu>
2013-11-07 01:34:42 -0800

    added hlp_setdiags

--- experimental/coreFunctions/grp/grp_mpa_prjGraphMetric.m
+++ experimental/coreFunctions/grp/grp_mpa_prjGraphMetric.m
@@ -1,10 +1,42 @@
 function varargout = grp_mpa_prjGraphMetric(varargin)
 % [STUDY obj] = grp_mpa_prjGraphMetric(STUDY,ALLEEG,...)
 %
-% perform causal projection analysis using the Measure Projection Toolbox
+% Perform causal projection analysis [1] using the Measure Projection Toolbox [2]
+%
 % Output: 
 %       STUDY:  modified STUDY with MPT dipoleAndMeasure object in STUDY.measureprojection.sift.object
 %       obj:    (optional) dipoleAndMeasure object
+% 
+% make sure that you have loaded STUDY in which all datasets have SIFT measures
+%
+% References: 
+% 
+% [1] Mullen T (2010) The Source Information Flow Toolbox (SIFT):
+%   Theoretical Handbook and User Manual. Chapter 6.9
+%   Available at: http://www.sccn.ucsd.edu/wiki/Sift
+% [2] Bigdely-Shamlo, N, Mullen, T, Kreutz-Delgado,  K, Makeig, S, 
+%   Measure Projection Analysis: A Probabilistic  Approach  to EEG Source Comparison and Multi-Subject Inference,
+%   NeuroImage (2013), Vol. 72, pp. 287-303, doi:   10.1016/j.neuroimage.2013.01.04
+%
+% Author: Tim Mullen 10-5-2013, SCCN/INC, UCSD. 
+% Email:  tim@sccn.ucsd.edu
+
+% This function is part of the Source Information Flow Toolbox (SIFT)
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 3 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 
 % extract some stuff from inputs for arg defaults
 ALLEEG = arg_extract(varargin,'ALLEEG',2);
@@ -20,16 +52,17 @@ else
     [freqRangeDef, timeRangeDef] = deal([]);
 end
 
-g = arg_define(varargin, ...
-        arg('STUDY',mandatory,[],'STUDY object'),  ...
-        arg('ALLEEG',mandatory,[],'ALLEEG array'), ...
-        arg({'connmethod','Estimator'},ConnNames,ConnNames,'Connectivity estimator to project'), ...
+g = arg_define([0 2],varargin, ...
+        arg_norep('STUDY',mandatory,[],'STUDY object'),  ...
+        arg_norep('ALLEEG',mandatory,[],'ALLEEG array'), ...
+        arg({'connmethod','Estimator'},ConnNames{1},ConnNames,'Connectivity estimator to project'), ...
         arg({'timeRange','TimeRange'},timeRangeDef,[],'[Min Max] Time range to smooth (sec). Leave blank to use all time points','shape','row','type','denserealdouble'), ...
         arg({'freqRange','FrequencyRange'},freqRangeDef,[],'[Min Max] Frequency range to smooth (Hz). Leave blank to use all frequencies','type','expression','shape','row'), ...
         arg({'collapseTime','CollapseTime'},false,[],'Average across time before smoothing'), ...
         arg({'collapseFreqs','CollapseFreqs'},true,[],'Integrate across frequencies before smoothing'), ...
         arg({'rejOutsideBrain','RejectDipolesOutsideBrain'},true,[],'Reject dipoles outside the brain'), ...
-        arg_sub({'graphMetric','GraphMetric'},@hlp_computeGraphMeasure,{},'Graph metric options','suppress',{'srcNodes','targNodes'}), ...
+        arg_sub({'graphMetric','GraphMetric'},{},@hlp_computeGraphMeasure,'Graph metric options','suppress',{'srcNodes','targNodes'}), ...
+        arg({'enableCache','EnableCaching'},true,[],'Enable caching of MPT object. This could use a lot of memory, but accelerate subsequent calls to this function'), ...
         arg({'verb','VerbosityLevel'},2,{int32(0) int32(1) int32(2)},'Verbosity level. 0 = no output, 1 = text, 2 = graphical') ...
     );
     
@@ -37,26 +70,32 @@ g = arg_define(varargin, ...
 if g.collapseTime && g.collapseFreqs
     error('At most one dimension can be collapsed. Otherwise, there is nothing to smooth!');
 end
-if any(cellfun(@(x) isempty(hlp_checkeegset(x,{'conn'})), ALLEEG))
+if any(arrayfun(@(x) ~isempty(hlp_checkeegset(x,{'conn'})), ALLEEG))
     error('You must first run SIFT analysis on all datasets in the STUDY');
 end
 
-waitbarstr = sprintf('Creating Measure Projection Object for %s',g.graphMetric.graphMetric);
+waitbarstr = ['Creating MPT Object for ',g.graphMetric.graphMetric];
 if g.verb==2
     % reset the color list
     hlp_getNextUniqueColor(hsv(10),'reset');
     multiWaitbar(waitbarstr,'Reset','Color',hlp_getNextUniqueColor);
 elseif g.verb==1
-    fprintf([waitbarstr '...']);
+    fprintf([waitbarstr '...\n']);
 end
 
-% make sure that you have loaded STUDY in which all datasets have SIFT measures
-dipoleAndMeasure = pr.dipoleAndMeasureOfStudySIFT(g.STUDY, g.ALLEEG);
+% create MPT object
+if g.enableCache
+    dipoleAndMeasure = hlp_microcache('grp_mpa',@pr.dipoleAndMeasureOfStudySIFT,g.STUDY, g.ALLEEG);
+else
+    dipoleAndMeasure = pr.dipoleAndMeasureOfStudySIFT(g.STUDY, g.ALLEEG);
+end
 % reject dipoles outside the brain
 if g.rejOutsideBrain
+    if g.verb, fprintf('Removing dipoles outside the brain\n'); end
     dipoleAndMeasure = dipoleAndMeasure.createSubsetInRelationToBrain();
 end
-    
+
+if g.verb, fprintf('Computing graph measures...\n'); end
 for sidx = 1:length(g.ALLEEG)
     % get indices of dipoles for this dataset
     dipidx = dipoleAndMeasure.datasetIdAllConditions==sidx;
@@ -76,19 +115,20 @@ for sidx = 1:length(g.ALLEEG)
 end
 
 % update some fields
-dipoleAndMeasure.time = nt;
+dipoleAndMeasure.time = nt*1000;
 dipoleAndMeasure.frequency = nf;
 dipoleAndMeasure.measureLabel = g.graphMetric.graphMetric;
 dipoleAndMeasure.numberOfMeasureDimensions = ndims(gm)-1;
 
 % store results and prepare outputs
+STUDY = g.STUDY;
 STUDY.measureprojection.sift.object =  dipoleAndMeasure;
 varargout{1} = STUDY;
 if nargout>1, varargout{2} = dipoleAndMeasure; end
 
 if g.verb==2
     multiWaitbar(waitbarstr,'Close');
-elseif g.verb==1, fprintf('\n'); end
+elseif g.verb==1, fprintf('done\n'); end
 
 
 % Helper functions
@@ -96,32 +136,35 @@ elseif g.verb==1, fprintf('\n'); end
 function [gm newtimes newfreqs] = ComputeGraphMeasure(Conn,g)
 % compute graph measures from a Connectivity object
 
+if ~iscell(g.connmethod)
+    g.connmethod = {g.connmethod};
+end
 
 % collapse connectivity matrices
 if g.collapseFreqs
     % collapse across freqs
-    Conn = hlp_filterConns(Conn,'connmethod',g.connmethod, ...
-        'method',{'freq','net'},'frange',g.freqRange,'freqdim',3,'timedim',4,'verb',g.verb);
+    Conn = hlp_filterConns(Conn,'connmethods',g.connmethod, ...
+        'method',{'freq','net'},'frange',g.freqRange,'freqdim',3,'timedim',4,'verb',0);
 else
     % only select subset of freqs
-    Conn = hlp_filterConns(Conn,'connmethod',g.connmethod, ...
+    Conn = hlp_filterConns(Conn,'connmethods',g.connmethod, ...
         'method',{'freq','shrinkonly'}, ...
-        'frange',g.freqRange, 'freqdim',3,'timedim',4,'verb',g.verb);
+        'frange',g.freqRange, 'freqdim',3,'timedim',4,'verb',0);
 end
 if g.collapseTime
     % collapse across time
-    Conn = hlp_filterConns(Conn,'connmethod',g.connmethod, ...
-        'method',{'time','mean'},'trange',g.timeRange,'freqdim',3,'timedim',4,'verb',g.verb);
+    Conn = hlp_filterConns(Conn,'connmethods',g.connmethod, ...
+        'method',{'time','mean'},'trange',g.timeRange,'freqdim',3,'timedim',4,'verb',0);
 else
     % only select subset of times
-    Conn = hlp_filterConns(Conn,'connmethod',g.connmethod, ...
+    Conn = hlp_filterConns(Conn,'connmethods',g.connmethod, ...
         'method',{'time','shrinkonly'}, ...
-        'trange',g.timeRange,'freqdim',3,'timedim',4,'verb',g.verb);
+        'trange',g.timeRange,'freqdim',3,'timedim',4,'verb',0);
 end
 newtimes = Conn.erWinCenterTimes;
 newfreqs = Conn.freqs;
 % compute graph measure
-gm = hlp_computeGraphMeasure('cmatrix',Conn.(g.connmethod),g.graphMetric);
+gm = hlp_computeGraphMeasure('cmatrix',Conn.(g.connmethod{1}),g.graphMetric);
 
 
 
@@ -152,7 +195,7 @@ gm = hlp_computeGraphMeasure('cmatrix',Conn.(g.connmethod),g.graphMetric);
 % gmProj = pr.meanProjection(dipoleAndMeasure, dipoleAndMeasure.getPairwiseFishersZSimilarity, pr.headGrid, 'stdOfDipoleGaussian', optimalGaussianWidth);
 % 
 % % project measures
-% gmProj = gmProj.createDomain(dipoleAndMeasure, g.mpaOpts, 0.05);
+% gmProj = gmProj.createDomain(gmProj, 0.9, 0.05);
 %     
 
 
