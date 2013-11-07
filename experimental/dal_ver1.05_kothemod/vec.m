8ea63b938b3c7b4e6b4c2c0fcb50724586d2c8c3
Tim Mullen <mullen.tim@gmail.com>
2013-11-06 22:17:45 -0800

    cleaned up repo

--- experimental/dal_ver1.05_kothemod/vec.m
+++
@@ -1,7 +0,0 @@
-% vec - vectorize an array
-%
-% Copyright(c) 2009-2011 Ryota Tomioka
-% This software is distributed under the MIT license. See license.txt
-function V=vec(M)
-% V=reshape(M, [numel(M), 1]);
-V = M(:);
