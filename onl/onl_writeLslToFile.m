bf4ab1c2b63297f5e58f186f6c489d645103d938
Tim Mullen <mullen.tim@gmail.com>
2014-01-23 18:50:45 -0800

    cleaned up and tweaked onl_writeLslToFile()

--- onl/onl_writeLslToFile.m
+++ onl/onl_writeLslToFile.m
@@ -13,19 +13,10 @@ function onl_writeLslToFile(varargin)
 %                        Input Data Type: string                                                        
 %                                                                                                       
 % OutputOptions:         Output options                                                                 
+%                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+%     | FileName:        The file to write to. This may also contain a system or platform-independent path              
 %                        Input Range  : Unrestricted                                                    
-%                        Default value: n/a                                                             
-%                        Input Data Type: string                                                        
-%                                                                                                       
-%     | SourceStream:    Input online stream                                                            
-%                        This is the stream that shall be written to disk.                              
-%                        Input Range  : Unrestricted                                                    
-%                        Default value: 'laststream'                                                     
-%                        Input Data Type: string                                                        
-%                                                                                                       
-%     | FileName:        The file name to write to                                                      
-%                        Input Range  : Unrestricted                                                    
-%                        Default value: 'lastdata.set'                                                    
+%                        Default value: 'data:/lastdata.set'                                                    
 %                        Input Data Type: string                                                        
 %                                                                                                       
 %     | UpdateFrequency: Update frequency                                                               
@@ -45,17 +36,14 @@ function onl_writeLslToFile(varargin)
 % See Also: run_writedataset(), run_readlsl()
 %
 % Tim Mullen, SCCN/INC/UCSD 2013
+% Inspired by BCILAB functions by Christian Kothe
 
 % make sure that everything is on the path and LSL is loaded
 if ~exist('arg_define','file')
     addpath(genpath(fileparts(mfilename('fullpath')))); end
-if ~exist('env_translatepath','file')
-    % standalone case
-    lib = lsl_loadlib();
-else
-    % if we're within BCILAB we want to make sure that the library is also found if the toolbox is compiled
-    lib = lsl_loadlib(env_translatepath('dependencies:/liblsl-Matlab/bin'));
-end
+% make sure that the library is also found if the toolbox is compiled
+lib = lsl_loadlib(env_translatepath('dependencies:/liblsl-Matlab/bin'));
+
 
 % handle default arguments
 try
@@ -67,7 +55,7 @@ end
 % handle inputs
 opts = arg_define(varargin, ...
         arg({'streamname','StreamName'},streamnames{1},streamnames,'LSL stream that should be written to disk. This is the name of the stream.'), ...
-        arg_sub({'outopts','OutputOptions'},{},@run_writedataset,'Output options','suppress',{'in_stream'}) ...
+        arg_sub({'outopts','OutputOptions'},{'out_filename','data:/lastdata.set'},@run_writedataset,'Output options','suppress',{'in_stream'}) ...
         );
 
 if isempty(opts.streamname)
@@ -81,19 +69,22 @@ else
     taken = evalin('base','whos(''lsl*'')');
     buffername = genvarname(['lsl_' opts.streamname '_stream'],{taken.name});
     
-    % create stream to read data from LSL
+    % create local stream to read data from LSL
     run_readlsl('MatlabStream',buffername,'SelectionProperty','name','SelectionValue',opts.streamname);
-
+    
     % write data to file
+    fprintf('Will write stream ''%s'' to %s\n',opts.streamname,env_translatepath(opts.outopts.out_filename));
+    fprintf('To end recording, type:\n>> clear %s \n',buffername);
     run_writedataset(opts.outopts,'in_stream',buffername)
-    fprintf('To end recording, type: onl_clear\n');
 end
-    % find names of streams on the lab network...
-    function names = find_streams
-        streams = lsl_resolve_all(lib,1);
-        names = cellfun(@(s)s.name(),streams ,'UniformOutput',false);
-        if isempty(names)
-            error('There is no stream visible on the network.'); end
 
-    end
+% find names of streams on the lab network...
+function names = find_streams
+    streams = lsl_resolve_all(lib,1);
+    names = cellfun(@(s)s.name(),streams ,'UniformOutput',false);
+    if isempty(names)
+        error('There is no stream visible on the network.'); end
+
+end
+
 end
