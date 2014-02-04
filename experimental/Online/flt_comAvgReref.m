c847ce971370dbebc9d3eb2db0f1224203775039
Tim Mullen <mullen.tim@gmail.com>
2014-02-03 19:48:27 -0800

    rem'd experimental

--- experimental/Online/flt_comAvgReref.m
+++
@@ -1,4 +0,0 @@
-function EEG = flt_comAvgReref(EEG,goodchannels)
-
-    % do common average rereferencing (excluding bad channels)
-    EEG.data = bsxfun(@minus,EEG.data,mean(EEG.data(goodchannels,:),1));
