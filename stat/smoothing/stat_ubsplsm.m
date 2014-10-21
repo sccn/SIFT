ea49c4034872335d78cb7e257f017d6d498ec862
Tim Mullen <mullen.tim@gmail.com>
2014-10-21 16:40:55 -0700

    stats: major upgrade to MCMC smoothing package: optimized code, added diagnostics, and improved stability.

--- stat/smoothing/stat_ubsplsm.m
+++ stat/smoothing/stat_ubsplsm.m
@@ -30,128 +30,143 @@ function [FIT MCMC_LastState]=stat_ubsplsm(varargin)
 
 arg_define([0 1],varargin, ...
     arg_norep({'Y','TSData'},mandatory,[],sprintf(['Cell array of data to smooth.\n' ...
-              'Generally, Y is a cell array where Y{i} is the T x 1 vector of time-varying (or freq-varying) connectivity for the kth channel pair of the sth subject.\n' ...
-              'e.g. Y = {s1(1,1) s1(1,2) s1(1,3) ... s1(N,1) s1(N,2) s1(N,3) ... \n' ...
-              '          s2(1,1) s2(1,2) s2(1,3) ... } \n'])), ...
+              'Generally, Y is a cell array where Y{i} is the [M x M x Q] matrix of M-channel connectivity for the ith subject.\n'])), ...
     arg({'smoothingLayout','MatrixElementsToSmooth'},{'diagonals','off-diagonals'},{'diagonals','off-diagonals'},'Which parts of the matrix to smooth. Diagonals (e.g. auto-connectivity) and off-diagonals (e.g. cross-connectivity) will be smoothed separately','type','logical'), ...
     arg_sub({'mcmc_opts','MCMC'},{}, ...
     {...
         arg_sub({'mcmc_init','InitMCMC'},{},@stat_ubsplsm_init,'Initialize MCMC','suppress','verb') ...
         arg_sub({'mcmc_run','RunMCMC'},{},@stat_ubsplsm_mcmc,'MCMC runtime options','suppress','verb') ...
     },'Perform Markov Chain Monte Carlo estimation'), ...
-    arg_norep({'MCMC_InitState'},struct([]),[],'Object containing initial state of Gibbs sampler. If supplied, this overrides mcmc_init'), ...
+    arg_norep({'MCMC_InitState'},[],[],'Object containing initial state of Gibbs sampler. If supplied, this overrides mcmc_init'), ...
     arg({'verb','VerbosityLevel'},2,{int32(0) int32(1) int32(2)},'Verbosity level. 0 = no output, 1 = text, 2 = graphical') ...
     );
     
 % initialize some vars
-init_mcmc = isempty(g.MCMC_InitState);    
+init_mcmc = isempty(MCMC_InitState);    
 nsubj=length(Y);
-
-% determine the number of variables for each subject
-m_i=zeros(nsubj,1);
-for i=1:nsubj
-    m_i(i)=size(Y{i},1);
-end
+% determine the number of sources for each subject
+M_i=cellfun(@(y) size(y,1),Y,'UniformOutput',true);
 
 % determining smoothing layout
-smoothDiags     = ismember_bc('diagonals',g.smoothingLayout);
-smoothOffDiags  = ismember_bc('off-diagonals',g.smoothingLayout);
+smoothDiags     = ismember_bc('diagonals',smoothingLayout);
+smoothOffDiags  = ismember_bc('off-diagonals',smoothingLayout);
     
 %% get smoothed time-varying connectivity coefficients
 
 % Diagonals and off-diagonals often have different variances, so we smooth
 % them separately
 
+% get linear indices of diagonal and off-diagonal
+%  elements of Y{si} conn tensor for each subject si
+[dg_inds, od_inds] = deal({});
+for si=1:nsubj
+    mi = M_i(si);
+    all_inds = 1:mi^2;
+    dg_inds{si} = sub2ind([mi mi],1:mi,1:mi);
+    od_inds{si} = setdiff_bc(all_inds,dg_inds{si});
+end
+% Note that we will "flatten" Y to cell array of form:
+% CPairs = {s1(1,1) s1(1,2) s1(1,3) ... s1(N,1) s1(N,2) s1(N,3) ...
+%           s2(1,1) s2(1,2) s2(1,3) ... }
+% where sk(i,j) is the 1 x Q vector of connectivity values from channels 
+% j to i, for the kth subject   
+
+waitbarstr = 'Smoothing...';
 % smooth diagonals
 % -------------------------------------------------------------------------
 if smoothDiags
+    if verb==1
+        fprintf(hlp_separator());
+        fprintf('Smoothing diagonals...\n');
+    end
     if verb==2
-        multiWaitbar('Smoothing Diagonals','Reset', ...
-                     hlp_getNextUniqueColor('reset'));
+        multiWaitbar(waitbarstr,'Reset', ...
+                     'Color',hlp_getNextUniqueColor('reset'));
     end
 
     % extract self-connectivity for each subject
-    CPairs=cell(sum(m_i),1);
-    ind=0;
-    for i=1:nsubj
-        for j=1:m_i(i)
-            ind=ind+1;
-            CPairs{ind}=squeeze(Y{i}(j,j,:));
-        end
-    end
+    % in "flattened" form
+    CPairs = flatten_tensor(Y,dg_inds);
 
     % initialize MCMC
     if init_mcmc
-        MCMC_InitState = stat_ubsplsm_init('Y',mcmc_opts.mcmc_init,'verb',verb);
+        MCMC_InitState_DG = hlp_microcache('ubsplsm',@stat_ubsplsm_init, ...
+                                               mcmc_opts.mcmc_init, ...
+                                               'T',length(CPairs{1}),'R',length(CPairs),...
+                                               'verb',verb);
+    else
+        MCMC_InitState_DG = MCMC_InitState.diags;
     end
     
-    % perform the smoothing
-    [fit_diag MCMC_LastState.diag]=stat_ubsplsm_mcmc('Y',CPairs,mcmc_opts.mcmc_run, ...
-                                        'verb',verb,'MCMC_InitState',MCMC_InitState.diag);
+    % perform the smoothing for all subjects jointly
+    [fit_dg, MCMC_LastState.diags]=stat_ubsplsm_mcmc('Y',CPairs,mcmc_opts.mcmc_run, ...
+                                  'verb',verb,'MCMC_InitState',MCMC_InitState_DG);
+else
+    MCMC_LastState.diags = struct([]);
 end
 
 
 % smooth off-diagonals
 % -------------------------------------------------------------------------
 if smoothOffDiags
-    if verb
-        multiWaitbar('Smoothing Off-Diagonals',1/3);
+    if verb==1
+        fprintf(hlp_separator());
+        fprintf('Smoothing off-diagonals...\n');
     end
-
-    % count the total number of pairs
-    m_offdiag=sum(m_i.^2-m_i);
-
+    if verb==2
+        multiWaitbar(waitbarstr,1/3);
+    end
+    
     % extract cross-connectivity off-diagonal
-    % elements for each subject
-    CPairs=cell(m_offdiag,1);
-    ind=0;
-    for i=1:nsubj
-        for j1=1:m_i(i)
-            for j2=1:m_i(i)
-                if j1~=j2
-                    ind=ind+1;
-                    CPairs{ind}=squeeze(Y{i}(j1,j2,:));
-                end
-            end
-        end
-    end
+    % elements for each subject in "flattened" form
+    CPairs = flatten_tensor(Y,od_inds);
 
     % initialize MCMC
     if init_mcmc
-        MCMC_InitState = stat_ubsplsm_init('Y',mcmc_opts.mcmc_init,'verb',verb);
+        MCMC_InitState_OD = hlp_microcache('ubsplsm',@stat_ubsplsm_init, ...
+                                            mcmc_opts.mcmc_init, ...
+                                            'T',length(CPairs{1}),'R',length(CPairs), ...
+                                            'verb',verb);
+    else
+        MCMC_InitState_OD = MCMC_InitState.offdiags;
     end
     
     % perform the smoothing
-    [fit_offdiag MCMC_LastState.offdiag]=stat_ubsplsm_mcmc('Y',CPairs,mcmc_opts.mcmc_run, ...
-                                         'verb',verb,'MCMC_InitState',MCMC_InitState.offdiag);
+    [fit_od, MCMC_LastState.offdiags]=stat_ubsplsm_mcmc('Y',CPairs,mcmc_opts.mcmc_run, ...
+                                     'verb',verb,'MCMC_InitState',MCMC_InitState_OD);
+else
+    MCMC_LastState.offdiags = struct([]);
 end
 
 
 % store data in [nchs x nchs x time x distribution]
-if verb
-    multiwaitbar('Creating final data matrices...',2/3);
+if verb==1
+    fprintf(hlp_separator());
+    fprintf('Constructing final matrices\n');
+end
+if verb==2
+    multiWaitbar(waitbarstr,2/3);
 end
 FIT = cell(1,nsubj);
 ind=0;
 ind_diag = 0;
 for i=1:nsubj
-    FIT{i} = zeros(m_i(i),m_i(i),size(fit_offdiag,1),niterToKeep);
-    
-    for j1=1:m_i(i)
-        for j2=1:m_i(i)
-            if j1~=j2
+    FIT{i} = zeros(M_i(i),M_i(i),size(fit_od,1),size(fit_od,3));
+    for j1=1:M_i(i)
+        for j2=1:M_i(i)
+            if smoothOffDiags && (j1~=j2)
                 ind=ind+1;
-                FIT{i}(j1,j2,:,:)=squeeze(fit_offdiag(:,ind,:));
-            elseif niters~=0
+                FIT{i}(j1,j2,:,:)=squeeze(fit_od(:,ind,:));
+            elseif smoothDiags && (j1==j2)
                 ind_diag = ind_diag+1;
-                FIT{i}(j1,j2,:,:)=squeeze(fit_diag(:,ind_diag,:));
+                FIT{i}(j1,j2,:,:)=squeeze(fit_dg(:,ind_diag,:));
             end
         end
     end
 end
 
-if verb
-    multiWaitbar('CloseAll');
+if verb==2
+    multiWaitbar(waitbarstr,'Close');
     pause(0.1);
 end
 
